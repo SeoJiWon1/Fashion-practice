8be2a5c5edfba1c7dcaa841e6ab51effd003106a
SeoJiWon1 <120435947+SeoJiWon1@users.noreply.github.com>
2022-12-13 18:53:10 +0900

    1

--- app.py
+++ app.py
@@ -20,34 +20,34 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
    st.header("Spring")
-   st.image("./1980 image/1980패션 봄.jpg")
+   st.image("./Fashion/1980 image/1980패션 봄.jpg")
 
 with col2:
    st.header("Summer")
-   st.image("./1980 image/1980패션 여름.jpg")
+   st.image("./Fashion/1980 image/1980패션 여름.jpg")
 with col3:
     st.header("Fall")
-    st.image("./1980 image/1980패션 가을.jpg")
+    st.image("./Fashion/1980 image/1980패션 가을.jpg")
 
 with col4:
     st.header("Winter")
-    st.image("./1980 image/1980패션 겨울.jpg")
+    st.image("./Fashion/1980 image/1980패션 겨울.jpg")
 
 st.markdown("### 2020's Fashion")
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
    st.header("Spring")
-   st.image("./2020 image/2020패션 봄.jpg")
+   st.image("./Fashion/2020 image/2020패션 봄.jpg")
 
 with col2:
    st.header("Summer")
-   st.image("./2020 image/2020패션 여름.jpg")
+   st.image("./Fashion/2020 image/2020패션 여름.jpg")
 
 with col3:
     st.header("Fall")
-    st.image("./2020 image/2020패션 가을.jpg")
+    st.image("./Fashion/2020 image/2020패션 가을.jpg")
 
 with col4:
     st.header("Winter")
-    st.image("./2020 image/2020패션 겨울.jpg")
+    st.image("./Fashion/2020 image/2020패션 겨울.jpg")
