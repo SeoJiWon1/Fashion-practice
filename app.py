374a0fb740ef54134bc01263decba1a795bc0e3a
SeoJiWon1 <120435947+SeoJiWon1@users.noreply.github.com>
2022-12-13 20:16:51 +0900

    1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('cj.csv', encoding = 'cp949')
+df = pd.read_csv('cju.csv', encoding = 'cp949')
 # 파일 저장할 때(저장할 데이터프레임 df로 지정)
 # df.to_csv('파일이름',encoding='cp949')
 # 파일 불러올때
