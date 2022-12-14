6ba6bd2a2dd70f7c907e55e6e61100e605aecb1c
SeoJiWon1 <120435947+SeoJiWon1@users.noreply.github.com>
2022-12-14 10:29:18 +0900

    3

--- Fashionapp,req/Fashion_app.py
+++ Fashionapp,req/Fashion_app.py
@@ -68,7 +68,7 @@ st.header("Size of the fashion market for all items")
 st.bar_chart(Fashion, width = 150, height = 600)
 
 
-import seaborn as sns
+
 # sns.histplot(data=Fashion, x='2022_SS')
 sns.countplot(data=Fashion, x="2021" )
 plt.title("타이타닉호의 각 클래스별, 승객 수")
