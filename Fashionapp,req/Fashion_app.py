a34663465430400be20e97e738e5805691675802
SeoJiWon1 <120435947+SeoJiWon1@users.noreply.github.com>
2022-12-14 12:25:45 +0900

    1234

--- Fashionapp,req/Fashion_app.py
+++ Fashionapp,req/Fashion_app.py
@@ -72,7 +72,7 @@ labels = ['Male_formal','Female_formal','Casual','Sprots','Inner','kids','Shoes'
 values = [1921,1359,8311,2697,964,584,3386]
 
 fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
-st.write(fig)
+st.plotly_chart(fig)
 
 
 
