6c928a1ae123b4bd96d4367146e7c980df28e0f8
SeoJiWon1 <120435947+SeoJiWon1@users.noreply.github.com>
2022-12-15 17:16:19 +0900

    1

--- ml_model/app.py
+++ ml_model/app.py
@@ -4,3 +4,11 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
+
+
+import joblib
+import os
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
+model = joblib.load(model_path)
+st.write("## 선형 회귀 모델")
+st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
